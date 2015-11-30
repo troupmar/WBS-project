973ea02ae1e7db7c22e76fd66ee67e622bcc647f
Martin Troup <troupmar@gmail.com>
2015-11-30 14:32:15 -0700

    Remote database added.

--- model/database_credentials.php
+++ model/database_credentials.php
@@ -2,10 +2,10 @@
 
 function get_local_database_credentials()
 {
-	return array('host' 	=> 'localhost',
-				 'database' => 'wbs_project',
-				 'username' => 'root',
-				 'password' => 'root');
+	return array('host' 	=> 'earth.cs.utep.edu',
+				 'database' => 'cs5339team3fa15',
+				 'username' => 'cs5339team3fa15',
+				 'password' => 'cs5339!cs5339team3fa15');
 }
 
 
